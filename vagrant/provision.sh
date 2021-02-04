2f6cd2e00b6361f2fc65088c1168cc77e2680cc7
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-02-04 14:46:11 +0100

    Add packages needed for operator to Vagrant machine

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get --yes upgrade
 
-apt-get install --yes make
+apt-get install --yes make gcc
 
 # Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -33,3 +33,19 @@ microk8s.kubectl config view --raw > /tailing-sidecar/.kube-config
 snap alias microk8s.kubectl kubectl
 
 usermod -a -G microk8s vagrant
+
+# Install go
+wget "https://golang.org/dl/go1.15.6.linux-amd64.tar.gz"
+tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+rm go1.15.6.linux-amd64.tar.gz
+
+
+# Install kubebuilder
+curl -L "https://go.kubebuilder.io/dl/2.3.1/linux/amd64" | tar -xz -C /tmp/
+mv /tmp/kubebuilder_2.3.1_linux_amd64 /usr/local/kubebuilder
+
+echo "export PATH=$PATH:/usr/local/kubebuilder/bin:/usr/local/go/bin" >> /home/vagrant/.bashrc
+
+# Install kustomize
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+mv kustomize /usr/local/bin/
